b7274f898d58b97ec5f6a94eb5ff2f35f0c947de
Matt <flyinv1@gmail.com>
2018-05-11 19:23:22 -0700

    Basic vector operations, camera vectors

--- algebra/vec3.py
+++ algebra/vec3.py
@@ -8,3 +8,11 @@ class Vec3:
     @staticmethod
     def zero():
         return Vec3(0, 0, 0)
+
+    def add(self, vec_1, vec_2):
+        return Vec3(vec_1.x + vec_2.x, vec_1.y + vec_2.y, vec_1.z + vec_2.z)
+
+    def multiply(self, vec_1, scalar):
+        return Vec3(vec_1.x * scalar, vec_1.y * scalar, vec_1.z * scalar)
+
+
--- engine/camera.py
+++ engine/camera.py
@@ -1,5 +1,6 @@
 from engine.node import Node
 from engine.transform import Transform
+from algebra.vec3 import Vec3
 
 
 class Camera(Node):
@@ -15,4 +16,14 @@ class Camera(Node):
         self.far_depth = far_depth
 
         # self.transform = transform
+
+    def get_screen_vectors(self):
+        vectors = []
+        v_1 = Vec3(self.width / 2, self.focal_length, self.height / 2)
+        v_2 = Vec3(-self.width / 2, self.focal_length, self.height / 2)
+        v_3 = Vec3(-self.width / 2, self.focal_length, -self.height / 2)
+        v_4 = Vec3(self.width / 2, self.focal_length, -self.height / 2)
+        return [v_1, v_2, v_3, v_4]
+
+
 
